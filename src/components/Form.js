310629866d3dc6ecc887c0f02b1e82f4d8c106e1
Juliane Alves <julianeju3654@gmail.com>
2022-01-10 15:00:32 -0300

    Nova tentativa do requisito

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,39 +6,73 @@ class Form extends React.Component {
       <form>
         Nome da carta:
         <label htmlFor="name-input-one">
-          <input id="name-input-one" type="text" data-testid="name-input" />
+          <input
+            id="name-input-one"
+            type="text"
+            data-testid="name-input"
+          />
         </label>
         Descrição dar carta:
-        <textarea data-testid="description-input" />
+        <textarea
+          data-testid="description-input"
+        />
         Primeiro atributo da carta:
         <label htmlFor="name-input-one">
-          <input id="name-input-one" type="number" data-testid="attr1-input" />
+          <input
+            id="name-input-one"
+            type="number"
+            data-testid="attr1-input"
+          />
         </label>
         Segundo atributo da carta:
         <label htmlFor="name-input-one">
-          <input id="name-input-one" type="number" data-testid="attr2-input" />
+          <input
+            id="name-input-one"
+            type="number"
+            data-testid="attr2-input"
+          />
         </label>
         Terceiro atributo da carta:
         <label htmlFor="name-input-one">
-          <input id="name-input-one" type="number" data-testid="attr3-input" />
+          <input
+            id="name-input-one"
+            type="number"
+            data-testid="attr3-input"
+          />
         </label>
         Caminho para a imagem:
         <label htmlFor="name-input-one">
-          <input id="name-input-one" type="text" data-testid="image-input" />
+          <input
+            id="name-input-one"
+            type="text"
+            data-testid="image-input"
+          />
         </label>
         Escolha da raridade da carta:
         <label htmlFor="rare-input">
-          <select data-testid="rare-input">
+          <select
+            data-testid="rare-input"
+            id="rare-input"
+          >
             <option value="normal">normal</option>
             <option value="raro">raro</option>
-            <option value="muito-raro">muito raro</option>
+            <option value="muito raro">muito raro</option>
           </select>
         </label>
         Inserir se a carta é super triunfo:
         <label htmlFor="name-input-one">
-          <input id="name-input-one" type="checkbox" data-testid="trunfo-input" />
+          <input
+            id="name-input-one"
+            type="checkbox"
+            data-testid="trunfo-input"
+          />
         </label>
-        <button type="button" data-testid="save-button"> Salvar </button>
+        <button
+          type="button"
+          data-testid="save-button"
+        >
+          Salvar
+        </button>
       </form>
     );
   }
